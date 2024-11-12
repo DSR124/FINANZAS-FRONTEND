cc89aca77414f8b8aa445189dc468d8c800db724
AnaJ123hib <anajcrus233@gmail.com>
2024-11-11 22:40:55 -0500

    main styles

--- src/app/components/empresa/lista-empresa/lista-empresa.component.ts
+++ src/app/components/empresa/lista-empresa/lista-empresa.component.ts
@@ -2,28 +2,27 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 import { MatSort, MatSortModule } from '@angular/material/sort';
-import { MatInputModule } from '@angular/material/input';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
-import { ActivatedRoute, RouterModule } from '@angular/router';
-import {MatDialog,MatDialogModule } from '@angular/material/dialog';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { Empresa } from '../../../models/empresa';
-import { EmpresaComponent } from '../empresa.component';
 import { EmpresaService } from '../../../services/empresa.service';
 import { LoginService } from '../../../services/login.service';
-import { MatCardModule } from '@angular/material/card';
-import { Router } from 'express';
-import { CommonModule } from '@angular/common';
 import { EmpresasUsuarioComponent } from '../empresas-usuario/empresas-usuario.component';
+import { CommonModule } from '@angular/common';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { RouterModule } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
 @Component({
   selector: 'app-lista-empresa',
   standalone: true,
   imports: [CommonModule, MatFormFieldModule, MatDialogModule , MatCardModule, MatInputModule, RouterModule, MatTableModule, MatPaginatorModule, MatSortModule, MatButtonModule, MatIconModule],
   templateUrl: './lista-empresa.component.html',
-  styleUrl: './lista-empresa.component.css'
+  styleUrls: ['./lista-empresa.component.css']
 })
-export class ListaEmpresaComponent implements OnInit{
+export class ListaEmpresaComponent implements OnInit {
   dataSource: Empresa[] = [];
   filteredDataSource: Empresa[] = [];
   filterValue: string = '';
@@ -31,6 +30,12 @@ export class ListaEmpresaComponent implements OnInit{
 
   @ViewChild(MatSort) sort!: MatSort;
   @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  // Array de nombres de archivos de imágenes
+  avatars = [
+    'avatar1.png', 'avatar2.png', 'avatar3.png', 'avatar4.png', 'avatar5.png',
+    'avatar6.png'
+  ];
 
   constructor(
     private empService: EmpresaService,
@@ -47,7 +52,12 @@ export class ListaEmpresaComponent implements OnInit{
     });
   }
 
-  // Filtra la lista de empresas por varios campos (nombre, usuario, dirección, ruc y tipo)
+  // Método para obtener el avatar basado en el índice
+  getAvatar(index: number): string {
+    return `/assets/${this.avatars[index % this.avatars.length]}`;
+  }
+
+  // Filtra la lista de empresas por varios campos
   filter(event: any) {
     this.filterValue = event.target.value.trim().toLowerCase();
     this.filteredDataSource = this.dataSource.filter((empresa) =>
